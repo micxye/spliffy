628deaf094edd17caf88f44ceaab0a649af1135d
Michael Ye <micxye@yahoo.com>
2018-11-06 13:17:02 -0800

    Updates

---
+++ precache-manifest.c1e9c4fc16d0ccf9496f97124d3d837e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a24ab564ea8069f15e68",
+    "url": "/spliffy/static/js/runtime~main.a24ab564.js"
+  },
+  {
+    "revision": "848404ca4ddde3d9b7a3",
+    "url": "/spliffy/static/js/main.848404ca.chunk.js"
+  },
+  {
+    "revision": "902879a40e0c389d3ca5",
+    "url": "/spliffy/static/js/1.902879a4.chunk.js"
+  },
+  {
+    "revision": "848404ca4ddde3d9b7a3",
+    "url": "/spliffy/static/css/main.2a694d16.chunk.css"
+  },
+  {
+    "revision": "ee1917bad3d1e9944547e934c60e64b1",
+    "url": "/spliffy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-gh-pages/precache-manifest.1a6b1d0e874ae8d7387c8188a13e9565.js"
+  "/spliffy/precache-manifest.c1e9c4fc16d0ccf9496f97124d3d837e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-gh-pages/index.html", {
+workbox.routing.registerNavigationRoute("/spliffy/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
